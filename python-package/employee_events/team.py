79b692680a944dbc86b8fa1c0e7c2ad18b65ca5f
Mohammed banabila <hamodibaemad@gmail.com>
2026-07-04 23:09:32 +0300

    done 50% of the project

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,58 +1,26 @@
-# Import the QueryBase class
-# YOUR CODE HERE
-
-# Import dependencies for sql execution
-#### YOUR CODE HERE
-
-# Create a subclass of QueryBase
-# called  `Team`
-#### YOUR CODE HERE
-
-    # Set the class attribute `name`
-    # to the string "team"
-    #### YOUR CODE HERE
+from .query_base import QueryBase
 
 
-    # Define a `names` method
-    # that receives no arguments
-    # This method should return
-    # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 5
-        # Write an SQL query that selects
-        # the team_name and team_id columns
-        # from the team table for all teams
-        # in the database
-        #### YOUR CODE HERE
-    
+class Team(QueryBase):
 
-    # Define a `username` method
-    # that receives an ID argument
-    # This method should return
-    # a list of tuples from an sql execution
-    #### YOUR CODE HERE
+    name = "team"
 
-        # Query 6
-        # Write an SQL query
-        # that selects the team_name column
-        # Use f-string formatting and a WHERE filter
-        # to only return the team name related to
-        # the ID argument
-        #### YOUR CODE HERE
+    def names(self):
+        return self.query("""
+            SELECT team_name,
+                   team_id
+            FROM team
+        """)
 
+    def username(self, id):
+        return self.query(f"""
+            SELECT team_name
+            FROM team
+            WHERE team_id = {id}
+        """)
 
-    # Below is method with an SQL query
-    # This SQL query generates the data needed for
-    # the machine learning model.
-    # Without editing the query, alter this method
-    # so when it is called, a pandas dataframe
-    # is returns containing the execution of
-    # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        return self.pandas_query(f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
@@ -63,4 +31,4 @@
                     WHERE {self.name}.{self.name}_id = {id}
                     GROUP BY employee_id
                    )
-                """
+        """)
